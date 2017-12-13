5404ee02a88409f3695bf99767302574a0f90ba9
Mark T. Holder <mtholder@gmail.com>
2017-12-13 18:16:00 -0500

    test now diffs with R output

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 td=../nexld/tests/example_nexml
+tdo=../nexld/tests/example_jsonld
 if ! test -d ${td} ; then
     echo "Skipping tests from nexld because ${td} was not found"
 else
@@ -8,17 +9,31 @@ else
     fi
     for fp in ${td}/*.xml ; do
         fn=$(basename "${fp}")
-        echo "Transforming ${fn} to JSON-LD"
-        python pynexld/__init__.py "${fp}" > "cruft/unnorm-$fn" 2> "cruft/err-$fn"
+        jfn=$(echo $fn | sed 's/.xml$/.json/')
+        echo "Transforming ${fn} to JSON-LD ..."
+        python pynexld/__init__.py "${fp}" > "cruft/unnorm-${jfn}" 2> "cruft/err-$fn" || exit
     done
-    if which jq 2>/dev/null ; then
+    if which jq >/dev/null ; then
         for fp in ${td}/*.xml ; do
-        fn=$(basename "${fp}")
-        echo "Normalizing JSON formatting of ${fn}"
-        cat "cruft/unnorm-$fn" | jq -S . > "cruft/jq-$fn"
-    done
+            fn=$(basename "${fp}")
+            jfn=$(echo $fn | sed 's/.xml$/.json/')
+            echo "Normalizing JSON formatting of cruft/unnorm-${jfn} ..."
+            cat "cruft/unnorm-${jfn}" | jq -S . > "cruft/jq-${jfn}"  || exit
+            rjqi="${tdo}/${jfn}"
+            if test -f "${rjqi}" ; then
+                rjqo=cruft/jq-r-${jfn}
+                if ! test -f ${rjqo} ; then
+                    echo "Normalizing JSON formatting of ${rjqi} ..."
+                    cat "${rjqi}" | jq -S . > "${rjqo}" || exit
+                fi
+                echo "Diffing ${rjqo} and cruft/jq-${jfn} ..."
+                diff "${rjqo}"  "cruft/jq-${jfn}" > "cruft/diff-R-Python-${jfn}.txt" 2>&1
+            else
+                echo "R output ${rjqi} does not exist"
+            fi
+        done
     else
         echo "Skipping transformation because jq is not found"
     fi
 fi
-python -m pytest
+python -m pytest || exit
